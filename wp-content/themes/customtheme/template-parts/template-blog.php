be345cbcf225136c9d137e496f5c8efda11477f6
ashim9 <ashim@echologyx.com>
2023-05-25 17:15:12 +0600

    theme setup done

---
+++ wp-content/themes/customtheme/template-parts/template-blog.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+* Template Name: Blog
+*/
+
+//get_header();
+
+?>
+
+<h1>Hello Ashim</h1>
---
+++ wp-content/themes/customtheme/template-parts/template-home.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+* Template Name: Home page
+*/
+
+get_header();
+
+?>
+
+<h1>Hello Ashim</h1>
+
+
+
+<?php
+get_footer();
+?>
