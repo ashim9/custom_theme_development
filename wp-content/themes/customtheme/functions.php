9f63938d80497b3b93333647bf0a1fafb2c6af32
ashim9 <ashim@echologyx.com>
2023-05-29 21:59:57 +0600

    loge service section added

--- wp-content/themes/customtheme/functions.php
+++ wp-content/themes/customtheme/functions.php
@@ -14,4 +14,37 @@ function wp_register_styles(){
 }
 add_action('wp_enqueue_scripts', 'wp_register_styles');
 
+
+
+if( function_exists('acf_add_options_page') ) {
+    
+    acf_add_options_page(array(
+        'page_title'    => 'Custom Theme Settings',
+        'menu_title'    => 'Custom Theme Settings',
+        'menu_slug'     => 'theme-general-settings',
+        'capability'    => 'edit_posts',
+        'redirect'      => false
+    ));
+}
+
+
+//enable svg uploading
+// Wp v4.7.1 and higher
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+$filetype = wp_check_filetype( $filename, $mimes );
+return [
+    'ext'             => $filetype['ext'],
+    'type'            => $filetype['type'],
+    'proper_filename' => $data['proper_filename']
+];
+
+}, 10, 4 );
+
+function cc_mime_types( $mimes ){
+$mimes['svg'] = 'image/svg+xml';
+return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
+
+
 ?>
--- wp-content/themes/customtheme/template-parts/template-home.php
+++ wp-content/themes/customtheme/template-parts/template-home.php
@@ -10,13 +10,17 @@ get_header();
 
 <?php
     $home_banner_contents = get_field('home_banner_contents');
-    $home_banner = get_field('home_banner');
+    // $home_banner = get_field('home_banner');
     $banner_gallery_card = get_field('banner_gallery_card');
+    $loge_service_title = get_field('loge_service_title');
+    $loge_service_subtitle = get_field('loge_service_subtitle');
+    $loge_service_card = get_field('loge_service_card');
+    $loge_service_right_options = get_field('loge_service_right_options');
 ?>
 
 <main>
     <?php if(isset($home_banner_contents)):?>
-        <div class="banner" style="background:url('<?php echo $home_banner_contents['home_banner'] ?>')no-repeat;background-size:cover">
+        <div class="banner" style="background:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url('<?php echo $home_banner_contents['home_banner'] ?>')no-repeat;background-size:cover">
             <div class="banner-contents">
                 <?php if(isset($home_banner_contents['home_banner_header'])): ?>
                     <h2><?php echo $home_banner_contents['home_banner_header'] ?></h2>
@@ -39,7 +43,7 @@ get_header();
     <?php if(isset($banner_gallery_card)): ?>
         <div class="banner_gallery_card">
             <?php foreach($banner_gallery_card as $banner_gallery_cards) {?>
-                 <div class="banner-gallery" style="background:url('<?php echo $banner_gallery_cards['banner_gallery_image'] ?>')no-repeat;background-size:cover">
+                 <div class="banner-gallery" style="background:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('<?php echo $banner_gallery_cards['banner_gallery_image'] ?>')no-repeat;background-size:cover">
                      <div class="banner-gallery-content">
                          <p><?php echo $banner_gallery_cards['banner_gallery_text'] ?></p>
                      </div>
@@ -47,6 +51,42 @@ get_header();
              <?php } ?>
         </div>
     <?php endif; ?>
+    <div class="loge-service-section">
+        <div class="container">
+            <div class="loge-service-title">
+                <?php if(isset($loge_service_title)):?>
+                    <h3><?php echo $loge_service_title ?></h3>
+                <?php endif; ?>
+                <?php if(isset($loge_service_subtitle)): ?>
+                    <p><?php echo $loge_service_subtitle ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="loge-service-card-wrapper">
+                <?php if(isset($loge_service_card)):
+                    foreach($loge_service_card as $loge_service_cards) {?>
+                        <div class="loge-service-card">
+                            <?php if(isset($loge_service_cards['loge_service_card_icon'])): ?>
+                                <img src="<?php echo $loge_service_cards['loge_service_card_icon'] ?>" alt="">
+                            <?php endif; ?>
+                            <?php if(isset($loge_service_cards['loge_service_card_title'])): ?>
+                                <p><?php echo $loge_service_cards['loge_service_card_title']?></p>
+                            <?php endif; ?>
+                            <?php if(isset($loge_service_cards['loge_service_card_descriptions'])): ?>
+                                <p class="loge_service_card_descriptions"><?php echo $loge_service_cards['loge_service_card_descriptions']?></p>
+                            <?php endif; ?>
+                        </div>
+                    <?php } ?>
+                <?php endif; ?>
+                <?php if(isset($loge_service_right_options)): ?>
+                    <ul>
+                        <?php foreach($loge_service_right_options as $loge_service_right_options_card){?>
+                            <li><?php echo $loge_service_right_options_card['loge_service_right_card_text'] ?></li>
+                        <?php } ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
